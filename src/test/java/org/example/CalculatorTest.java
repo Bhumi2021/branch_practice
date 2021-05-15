8629a47f6dfffd1fe5ea0e466b39987d7a7b0e6c
Bhumi2021 <abpatel20817@gmail.com>
2021-05-15 16:30:40 +0100

    Initial commit

--- src/test/java/org/example/CalculatorTest.java
+++ src/test/java/org/example/CalculatorTest.java
@@ -7,4 +7,8 @@ public class CalculatorTest {
     public void it_should_added_two_number(){
         System.out.println();
     }
+    @Test
+    public void it_should_subtract_two_number(){
+        System.out.println();
+    }
 }
