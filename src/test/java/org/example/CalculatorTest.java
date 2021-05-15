ee4a75097f91191ec70a608116f6c465653fc979
Bhumi2021 <abpatel20817@gmail.com>
2021-05-15 16:43:22 +0100

    add method

--- src/test/java/org/example/CalculatorTest.java
+++ src/test/java/org/example/CalculatorTest.java
@@ -7,8 +7,6 @@ public class CalculatorTest {
     public void it_should_added_two_number(){
         System.out.println();
     }
-    @Test
-    public void it_should_subtract_two_number(){
-        System.out.println();
-    }
+
 }
+
